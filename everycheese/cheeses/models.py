4ae629542c4c9d07d0c34a712a74f95a0b9d9767
Lorenzo Cesconetto <lorenzo_cesconeto@hotmail.com>
2020-07-30 10:41:28 -0300

    Use django-crispy-forms for better Add Cheese form display

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -8,20 +8,20 @@ from model_utils.models import TimeStampedModel
 
 class Cheese(TimeStampedModel):
     class Firmness(models.TextChoices):
-        UNSPECIFIED = "unspecified", "Unspecified"
-        SOFT = "soft", "Soft"
-        SEMI_SOFT = "semi-soft", "Semi-Soft"
-        SEMI_HARD = "semi-hard", "Semi-Hard"
-        HARD = "hard", "Hard"
+        UNSPECIFIED = 'unspecified', 'Unspecified'
+        SOFT = 'soft', 'Soft'
+        SEMI_SOFT = 'semi-soft', 'Semi-Soft'
+        SEMI_HARD = 'semi-hard', 'Semi-Hard'
+        HARD = 'hard', 'Hard'
 
-    name = models.CharField("Name of Cheese", max_length=255)
-    slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
-    description = models.TextField("Description", blank=True)
-    firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
-    country_of_origin = CountryField("Country of Origin", blank=True)
+    name = models.CharField('Name of Cheese', max_length=255)
+    slug = AutoSlugField('Cheese Address', unique=True, always_update=False, populate_from='name')
+    description = models.TextField('Description', blank=True)
+    firmness = models.CharField('Firmness', max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    country_of_origin = CountryField('Country of Origin', blank=True)
 
     def get_absolute_url(self):
-        """Return absolute URL to the Cheese Detail page."""
+        '''Return absolute URL to the Cheese Detail page.'''
         return reverse('cheeses:detail', kwargs={'slug': self.slug})
 
     def __str__(self) -> str:
