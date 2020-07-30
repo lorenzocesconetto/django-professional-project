eb0c3aadfc2110bc26bbc174dd0f3a53c8db1dc9
Lorenzo Cesconetto <lorenzo_cesconeto@hotmail.com>
2020-07-29 22:10:21 -0300

    Implement cheese country of origin

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -8,8 +8,6 @@ pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    # cheese = Cheese.objects.create(
-    #     name="Stracchino", description="Semi-sweet cheese that goes well with starches.", firmness=Cheese.Firmness.SOFT)
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
