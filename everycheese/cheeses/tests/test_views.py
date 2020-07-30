6b37aab3b7ae7649755e32d548d88cd0a2d32084
Lorenzo Cesconetto <lorenzo_cesconeto@hotmail.com>
2020-07-30 17:42:31 -0300

    Write tests for all cheese URL patterns

--- everycheese/cheeses/tests/test_views.py
+++ everycheese/cheeses/tests/test_views.py
@@ -15,22 +15,22 @@ from .factories import CheeseFactory
 pytestmark = pytest.mark.django_db
 
 
-# def test_good_cheese_list_view_expanded(rf):
-#     """For reference only"""
-#     # Determine the URL
-#     url = reverse("cheeses:list")
-#     # rf is pytest shortcut to django.test.RequestFactory
-#     # We generate a request as if from a user accessing
-#     # the cheese list view
-#     request = rf.get(url)
-#     # Call as_view() to make a callable object
-#     # callable_obj is analogous to a function-based view
-#     callable_obj = CheeseListView.as_view()
-#     # Pass in the request into the callable_obj to get an # HTTP response served up by Django
-#     response = callable_obj(request)
-#     # Test that the HTTP response has 'Cheese List' in the
-#     # HTML and has a 200 response code
-#     assertContains(response, 'Cheese List')
+def test_good_cheese_list_view_expanded(rf):
+    """For reference only, is redundant with funciton test_good_cheese_list_view()"""
+    # Determine the URL
+    url = reverse("cheeses:list")
+    # rf is pytest shortcut to django.test.RequestFactory
+    # We generate a request as if from a user accessing
+    # the cheese list view
+    request = rf.get(url)
+    # Call as_view() to make a callable object
+    # callable_obj is analogous to a function-based view
+    callable_obj = CheeseListView.as_view()
+    # Pass in the request into the callable_obj to get an # HTTP response served up by Django
+    response = callable_obj(request)
+    # Test that the HTTP response has 'Cheese List' in the
+    # HTML and has a 200 response code
+    assertContains(response, 'Cheese List')
 
 
 def test_good_cheese_list_view(rf):
