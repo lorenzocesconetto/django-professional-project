6456c1acd962b1fd4767059f00a0caea3619eaab
Lorenzo Cesconetto <lorenzo_cesconeto@hotmail.com>
2020-07-29 21:26:38 -0300

    Add CheeseFactory in a test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,14 +1,15 @@
 import pytest
 
 from ..models import Cheese
-
+from .factories import CheeseFactory
 
 # Connect to database
 pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino", description="Semi-sweet cheese that goes well with starches.", firmness=Cheese.Firmness.SOFT)
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    # cheese = Cheese.objects.create(
+    #     name="Stracchino", description="Semi-sweet cheese that goes well with starches.", firmness=Cheese.Firmness.SOFT)
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
