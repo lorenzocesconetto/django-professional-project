7548376888a039922ff241625ba3fe50b3611822
Lorenzo Cesconetto <lorenzo_cesconeto@hotmail.com>
2020-07-30 11:10:51 -0300

    Add creator field to Cheese model and custom migration

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField, country_to_text
 from django.urls import reverse
+from django.conf import settings
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
@@ -19,6 +20,7 @@ class Cheese(TimeStampedModel):
     description = models.TextField('Description', blank=True)
     firmness = models.CharField('Firmness', max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField('Country of Origin', blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
 
     def get_absolute_url(self):
         '''Return absolute URL to the Cheese Detail page.'''
